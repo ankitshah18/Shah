e6d1c69f7fc271615d51f83757c879ad3f19300f
ankitshah18 <121596533+ankitshah18@users.noreply.github.com>
2023-03-15 19:25:32 +0530

    Add files via upload

---
+++ Cocktail/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+    </section>
+  )
+}
+
+export default About
